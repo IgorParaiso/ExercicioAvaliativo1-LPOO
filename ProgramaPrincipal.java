26105585b7d1ee0b359a5d05d2dd4ac10aed8f74
PabloBarbato <101756427+PabloBarbato@users.noreply.github.com>
2022-03-16 20:15:50 -0300

    Update ProgramaPrincipal.java

--- ProgramaPrincipal.java
+++ ProgramaPrincipal.java
@@ -1,4 +1,34 @@
 public class ProgramaPrincipal {
+    
+package estatico;
+
+import io.InOut;
+
+public class AppPessoa {
+	public static void main (String[] args) {
+		Pesssoa[] vetPessoa = new Pessoa[5];
+		
+		InserirPessoa (vetPessoa);
+		Remover (vetPessoa);
+		Nome (vetPessoa);
+		Dia/Mês (vetPessoa);
+		Mês (vetPessoa);
+	}
+
+	private static void lerPessoa(Pessoa[] vetPessoa) {
+		String nome;
+		double nome, mes, mesDia, VetPessoa;
+		for (int i=0; i<vetPessoa.length; i++) {
+			nome = InOut.leString("Nome");
+			mes = InOut.leDouble("Mes");
+			mesDia = InOut.leDouble("Dia e Mês");
+			vetPessoa[i] = new Pessoa(nome, mes, mesDia);
+		}
+	}
+
+	
+}
+
 
     /**
      * @param args the command line arguments
