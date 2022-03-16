53a0fddf6612b25935025ef66d4f9882dd7cfa8d
Bruno-Balla <101756404+Bruno-Balla@users.noreply.github.com>
2022-03-16 20:14:17 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,3 +1,21 @@
 public class Pessoa {
      
+     private String nome;
+	private String telefone;
+	private String email;
+	private String dataDeAniversario;
+
+	public Pessoa (String nome, String telefone, String email, String dataDeAniversario){
+		this.nome = nome;
+		this.telefone = telefone;
+		this.email = email;
+		this.dataDeAniversario = dataDeAniversario;
+	}
+
+	public String getNome(){
+		return nome;
+	}
+	public void setNome(String nome){
+		this.nome = nome;    
+	}
 }
