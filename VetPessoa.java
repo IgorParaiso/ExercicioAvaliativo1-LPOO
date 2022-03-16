541c18260dad395efc961e03c20438516cab497c
IgorParaiso <59921521+IgorParaiso@users.noreply.github.com>
2022-03-16 20:18:33 -0300

    Update VetPessoa.java
    Cria os atributos básicos e o getPos e o getQuant

--- VetPessoa.java
+++ VetPessoa.java
@@ -1,3 +1,23 @@
 public class VetPessoa {
     
+    
+    private Pessoa[] vetPessoa;
+    private int quant;
+    
+    public VetPessoa (int tamanho){
+        this.vetPessoa = new Pessoa[tamanho];
+        this.quant = 0;
+    }
+    
+    public int getQuant(){
+        return quant;
+    }
+    
+    public Pessoa getPos(int posicao){
+        if (posicao >= 0 && posicao<quant){
+            return this.vetPessoa[posicao];
+        }
+        return null;
+    }
+    
 }
