bd97a76a0ef1b7f17ce2f0aa4ec7c5cbf9d77a84
jonesallanj <92412497+jonesallanj@users.noreply.github.com>
2022-03-16 20:12:50 -0300

    Update Data.java

--- Data.java
+++ Data.java
@@ -1,4 +1,32 @@
 public class Data {
-    public int ano;
-    public int nome;
+    private int dia;
+	private int mes;
+	private  int ano;
+
+	public Data (int dia, int mes, int ano){
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	} 
+	public int getDia(){
+		return dia;
+	}
+	public void SetDia(int d){
+		this.dia = d;
+	}
+	public int getMes(){
+		return mes;
+	}
+	public void SetMes (int m){
+		this.mes = m;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void SetAno (int a){
+		this.ano = a;
+	}
+	public String toString(){
+}
+
 }
